6f57abbe0f2bb47a8e3b2066e8407dfc10be75c6
Bret Comnes <bcomnes@gmail.com>
2014-12-27 10:58:14 -0800

    First start at rewriting module.

--- index.js
+++ index.js
@@ -8,151 +8,38 @@ var gits = require('quick-git');
 
 // Pass in the settings to get a new Repo object
 function Repo(obj) {
-  for (var key in obj) {
-    this[key] = obj[key];
-  }
+  var self;
+  var git = gits(obj.worktree);
+  self = git;
 
-  this.git = new Git(this.worktree)
+  function clone(cb) {
+    if (obj.remote) git.clone(obj.remote, cb);
+    else; // TODO new Error then cb(err);
+  };
+  self.clone = clone;
+
+  function pull(cb) {
+    git(['pull'], {
+      timeout: 15000
+    }, cb);
+  };
+  self.pull = pull;
+
+  function setUser(cb) {
+    git(['config', 'user.name', obj.name || 'bepo user'], cb)
+  };
+  self.setUser = setUser;
+
+  function setEmail(cb) {
+    git(['config', 'user.email', obj.email], cb)
+  };
+  self.setEmail = setEmail;
+
+  function add(files, cb) {
+    var add = ['add'].concat(files || ['-A'])
+    git(add, cb);
+  };
+  self.add = add;
+
+  return self;
 }
-
-var folders = ['media', '_posts'];
-var comMsg = ["'gitpub posted a new post'"];
-
-Repo.prototype.clone = function(cb) {
-
-  // TODO Audit how safe this is....
-  // Caution!  Passwords!
-  //
-  debug('Cloning to ' + this.worktree);
-
-  var repoUrl = url.parse(this.repo);
-
-  // TODO: Dont use env vars
-  if (process.env.ENV !== 'development') {
-    repoUrl.auth = process.env.USERNAME + ':' + process.env.PASSWORD;
-  }
-
-  var clone = ['git clone', url.format(repoUrl), this.worktree].join(' ');
-  cp.exec(clone, {
-    timeout: 15000
-  }, function cloneCb(err, stdout, stderr) {
-    if (err && cb) cb(err + stderr);
-    if (stdout) debug(stdout);
-    debug('Done cloning ' + this.repo)
-    cb();
-  }.bind(this));
-};
-
-Repo.prototype.pull = function(cb) {
-
-  debug('Pulling ' + this.worktree);
-
-  this.git(['pull', this.remoteBranch], {
-    timeout: 15000
-  }, function pullCb(err, stdout, stderr) {
-    if (err) cb(err);
-    if (stdout) debug(stdout);
-    debug('Finished pulling repo');
-    cb();
-  })
-}
-
-Repo.prototype.setUser = function(cb) {
-  debug(this.repo + ' User: ' + this.name || 'GitPub');
-
-  this.git(['config', 'user.name', this.name || 'GitPub'],
-    function setUserCb(err, stdout, stderr) {
-      if (err) cb(err);
-      if (stdout) debug(stdout);
-      debug('User Set');
-      cb();
-    }
-  )
-};
-
-Repo.prototype.setEmail = function(cb) {
-  debug(this.repo + ' email: ' + this.email);
-
-  this.git(['config', 'user.email', this.email],
-    function setEmailCb(err, stdout, stderr) {
-      if (err) cb(err);
-      if (stdout) debug(stdout);
-      debug('Email Set');
-      cb();
-    }
-  )
-};
-
-Repo.prototype.add = function(files) {
-  // folders to commit:
-  // folders = ['media', '_posts/ownyourgram']
-  var add = ['add'].concat(files || ['-A'])
-  return function addCls(cb) {
-    this.git(add, function addCb(err, stdout, stderr) {
-      if (err) cb(err);
-      if (stdout) debug(stdout);
-      debug('Added ' + files || '-A');
-      cb();
-    })
-  }.bind(this);
-};
-
-Repo.prototype.commit = function(msg) {
-  // comMsg: commit message
-  // pitfall: "'Ownyourgram posted a file'"
-  // The string must be delimited
-  return function commitCls(cb) {
-    this.git(['commit', '-m', msg],
-      function commitCb(err, stdout, stderr) {
-        if (err) cb(err);
-        if (stdout) debug(stdout);
-        debug('Changes committed');
-        debug(msg);
-        cb();
-      })
-  }.bind(this);
-}
-
-Repo.prototype.push = function(cb) {
-  debug('Pushing to ' + this.remoteBranch);
-
-  this.git(['push', this.remoteBranch], function pushCb(err, stdout, stderr) {
-    if (err) cb(err);
-    if (stdout) debug(stdout);
-    debug('Push Succeeded');
-    cb();
-  })
-};
-
-Repo.prototype.publish = function(cb) {
-  series([
-    this.pull,
-    this.add,
-    this.commit,
-    this.push
-  ], this, cb);
-};
-
-Repo.prototype.config = function(cb) {
-  series([
-    this.setUser,
-    this.setEmail
-  ], this, cb)
-};
-
-
-Repo.prototype.init = function(cb) {
-  fs.exists(this.gitdir, function(exists) {
-    if (exists === true) {
-      debug(that.github.repo + ' aready cloned.  Pulling.');
-      this.pull(cb);
-    } else {
-      async.series([
-        this.clone,
-        this.config,
-      ], cb).bind(this)
-    }
-  }.bind(this));
-};
-
-module.exports = Repo;
